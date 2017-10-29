33dfb16101f7a40a00f0cc77bdc1ef89f596f936
yueyy <31062615+yueyy@users.noreply.github.com>
2017-10-30 00:17:44 +0800

    Create 389-findTheDifference.java

---
+++ 389-findTheDifference.java
@@ -0,0 +1,34 @@
+import java.lang.reflect.Array;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class Solution
+{
+    public char findTheDifference(String s, String t)
+    {
+    // 想不出
+        for (int i = 0; i < ; i++) {
+            
+        }
+
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Input:");
+        String a = in.nextLine();
+        char[] aChar = a.toCharArray();
+
+        List<Character> aList = Array.asList(aarray);
+        Collections.shuffle(aList);
+        Character [] b = (Character[]) aList.toArray();
+        
+        String b = new String();
+
+        Solution solution = new Solution();
+        System.out.println(solution.findTheDifference(a,b));
+    }
+}
+
